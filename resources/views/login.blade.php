f1b15763c6e0aadb5c184ba7746ec1797ce90140
Savayi <savayichelsea90@gmail.com>
2023-10-23 22:20:37 +0300

    more changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -27,7 +27,7 @@
                 </div>
                 <button>Log In</button>
                 <div class="register">
-                    <p>Don't have an account? <a href="#"> Register</a></p>
+                    <p>Don't have an account? <a href="welcome.blade.php"> Register</a></p>
                 </div>
             </form>
         </div>
