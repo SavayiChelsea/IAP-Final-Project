83b45d4f34273a757dab23d8d0931608c848c833
ShadrachAroni <126606301+ShadrachAroni@users.noreply.github.com>
2023-11-24 22:27:51 +0300

    A few changes

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <div class="container">
         <h1 class="mt-6 mb-6">Invoices</h1>
         <ul class="nav nav-tabs">
