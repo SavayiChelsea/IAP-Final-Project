39527ae160aea0d7a9b380ed10c2cb636c214c32
ShadrachAroni <126606301+ShadrachAroni@users.noreply.github.com>
2023-11-26 23:19:53 +0300

    ,

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,13 +12,14 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/landingstyles.css') }}">
      
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         
             @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                <div class="landing-links">
 
                 {!! __(' :about_us  :contact_us', [
                                         'about_us' => '<a  target="_blank"  href="'.route('about.show').'" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" style="margin-right: 20px;">'.__('About Us').'</a>',
@@ -42,3 +43,7 @@
         </div>
     </body>
 </html>
+
+
+
+
