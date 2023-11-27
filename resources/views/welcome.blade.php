899c993aa056fb855b16b227b3da1e66b86861dc
ShadrachAroni <126606301+ShadrachAroni@users.noreply.github.com>
2023-11-27 10:10:07 +0300

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,52 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<<<<<<< HEAD
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Car Park</title>
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-</head>
-<body class="antialiased">
-    <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-
-        @if (Route::has('login'))
-            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                {!! __(':about_us :contact_us :services :FAQs', [
-                    'about_us' => '<a href="' . route('about.show') . '" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" style="margin-right: 20px;">' . __('About Us') . '</a>',
-                    'contact_us' => '<a href="' . route('contact.show') . '" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" style="margin-right: 20px;">' . __('Contact Us') . '</a>',
-                    'services' => '<a href="' . route('services') . '" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" style="margin-right: 20px;">' . __('Services') . '</a>',
-                    'FAQs' => '<a href="' . route('FAQs') . '" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" style="margin-right: 20px;">' . __('FAQs') . '</a>',
-                ]) !!}
-                @auth
-                    <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                @else
-                    <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-
-        <div class="banner">
-            <video autoplay loop muted playsinline>
-                <source src="{{ asset('video/carvid2.mp4') }}" type="video/mp4">
-            </video>
-            <div class="navbar">
-                <ul>
-                    <li><a href="{{ url('Main.html') }}">Home</a></li>
-                    <li><a href="{{ url('services.html') }}">Services</a></li>
-                    <li><a href="{{ url('FAQ.html') }}">FAQ's</a></li>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                </ul>
-            </div>
-=======
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -107,7 +60,6 @@
             @endif
 
         </div>
->>>>>>> 7fe2375acd4e61e9bac29a02630bb0e15cd7fcd4
         </div>
 
     </div>
