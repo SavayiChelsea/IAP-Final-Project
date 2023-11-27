7fe2375acd4e61e9bac29a02630bb0e15cd7fcd4
ShadrachAroni <126606301+ShadrachAroni@users.noreply.github.com>
2023-11-27 09:41:18 +0300

    .

---
+++ resources/markdown/faqs.md
@@ -0,0 +1,21 @@
+
+
+## FAQs
+
+### Frequently Asked Questions
+
+#### Q: How do I reserve a parking space?
+
+A: You can easily reserve a parking space through our online platform. Log in to your account, select your desired date and time, and choose an available parking spot.
+
+#### Q: What payment options are available?
+
+A: We offer various payment options, including credit/debit cards, mobile payments, and other convenient methods. You can select your preferred payment option during the reservation process.
+
+#### Q: Is my payment information secure?
+
+A: Yes, we prioritize the security of your payment information. Our platform uses encryption and follows industry best practices to ensure the confidentiality of your data.
+
+#### Q: Can I modify or cancel my reservation?
+
+A: Yes, you can modify or cancel your reservation through your account. Please check our cancellation policy for any applicable terms.
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,16 +12,36 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/landingstyles.css') }}">
-     
+        <style>
+    .video-container {
+        position: relative;
+        width: 100vw;
+        height: 100vh;
+        overflow: hidden;
+    }
+
+    video {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover; /* This property ensures the video covers the whole container while maintaining its aspect ratio */
+    }
+</style>
     </head>
     <body class="antialiased">
+   
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-        
+        <div class="video-container">
+    <video autoplay loop muted playsinline>
+    <source src="{{ asset('carvid2.mp4') }}" type="video/mp4">
+    </video>
             @if (Route::has('login'))
-                <div class="landing-links">
+                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
 
-                {!! __(' :about_us  :contact_us', [
+                {!! __(' :faqs :about_us  :contact_us' , [
+                                      'faqs' => '<a  target="_blank"  href="'.route('faqs.show').'" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" style="margin-right: 20px;">'.__('FAQs').'</a>',
                                         'about_us' => '<a  target="_blank"  href="'.route('about.show').'" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" style="margin-right: 20px;">'.__('About Us').'</a>',
                                         'contact_us' => '<a target="_blank" href="'.route('contact.show').'" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" style="margin-right: 20px;">'.__('Contact Us').'</a>',
                                 ]) !!}
@@ -39,11 +59,10 @@
                 </div>
             @endif
 
-
+        </div>
         </div>
     </body>
 </html>
-
 
 
 
