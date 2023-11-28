50a1d5aedabc2b39020563c31f98b28ae3c0dd48
SavayiChelsea <savayichelsea90@gmail.com>
2023-11-29 00:13:33 +0300

    Some changes

--- app/Http/Controllers/MPESAController.php
+++ app/Http/Controllers/MPESAController.php
@@ -5,50 +5,57 @@ namespace App\Http\Controllers;
 use App\Mpesa\STKPush;
 use App\Models\MpesaSTK;
 use Illuminate\Http\Request;
-//import the Facade
 use Iankumu\Mpesa\Facades\Mpesa;
-use App\Http\Controllers\Controller;
 
 class MPESAController extends Controller
 {
     public $result_code = 1;
-    public $result_desc = 'An error occured';
+    public $result_desc = 'An error occurred';
 
-    // Initiate  Stk Push Request
+    // Initiate Stk Push Request
     public function STKPush(Request $request)
     {
+        try {
+            $amount = 1;
+            $phoneno = $request->input('phone-number');
+            $account_number = 174379;
 
-        $amount = 1;
-        $phoneno = $request->input('phone-number');
-        $account_number = 174379;
+            $response = Mpesa::stkpush($phoneno, $amount, $account_number, $callbackurl = env('MPESA_CALLBACK_URL'));
+            $result = json_decode((string)$response, true);
 
-        $response = Mpesa::stkpush($phoneno, $amount, $account_number,$callbackurl = env('MPESA_CALLBACK_URL'));
-        $result = json_decode((string)$response, true);
+            if (isset($result['MerchantRequestID'], $result['CheckoutRequestID'])) {
+                MpesaSTK::create([
+                    'merchant_request_id' =>  $result['MerchantRequestID'],
+                    'checkout_request_id' =>  $result['CheckoutRequestID']
+                ]);
 
-
-        MpesaSTK::create([
-            'merchant_request_id' =>  $result['MerchantRequestID'],
-            'checkout_request_id' =>  $result['CheckoutRequestID']
-        ]);
-
-        return $result;
-    }
-
-
-    public function STKConfirm(Request $request)
-    {
-        $stk_push_confirm = (new STKPush())->confirm($request);
-
-        if ($stk_push_confirm) {
-
-            $this->result_code = 0;
-            $this->result_desc = 'Success';
+                $this->result_code = 0;
+                $this->result_desc = 'Success';
+            } else {
+                // Log or handle the case where the expected keys are not present in the response.
+            }
+        } catch (\Exception $e) {
+            // Log or handle the exception as needed.
         }
+
         return response()->json([
             'ResultCode' => $this->result_code,
             'ResultDesc' => $this->result_desc
         ]);
     }
 
-    
+    public function STKConfirm(Request $request)
+    {
+        $stk_push_confirm = (new STKPush())->confirm($request);
+
+        if ($stk_push_confirm) {
+            $this->result_code = 0;
+            $this->result_desc = 'Success';
+        }
+
+        return response()->json([
+            'ResultCode' => $this->result_code,
+            'ResultDesc' => $this->result_desc
+        ]);
+    }
 }
